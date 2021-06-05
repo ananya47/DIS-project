5f114f0846da6fa7acb2a4214abb68357a196787
Manish Agrawal <32239632+magrawal23@users.noreply.github.com>
2021-06-04 21:13:00 -0400

    Updated solution to dotnet 5.0. Included StackOverflow references for convenience.

--- MVC_EF_Start/Startup.cs
+++ MVC_EF_Start/Startup.cs
@@ -4,57 +4,63 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using MVC_EF_Start.DataAccess;
+// https://stackoverflow.com/a/58072137/1385857
+using Microsoft.Extensions.Hosting;
 
 namespace MVC_EF_Start
 {
-  public class Startup
-  {
-    public Startup(IConfiguration configuration)
+    public class Startup
     {
-      Configuration = configuration;
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Setup EF connection
+            // https://stackoverflow.com/a/43098152/1385857
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration["Data:IEXTrading:ConnectionString"]));
+
+            // added from MVC template
+            //services.AddMvc();
+            // https://stackoverflow.com/a/58772555/1385857
+            services.AddMvc(option => option.EnableEndpointRouting = false);
+        }
+
+        // this is the version from the MVC template
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            //This ensures that the database and tables are created as per the Models.
+            using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+            {
+                var context = serviceScope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+                context.Database.EnsureCreated();
+            }
+
+            // https://stackoverflow.com/a/58072137/1385857
+            if (env.IsDevelopment())
+            {
+                //app.UseBrowserLink();
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+              name: "default",
+              template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
     }
-
-    public IConfiguration Configuration { get; }
-
-    // This method gets called by the runtime. Use this method to add services to the container.
-    // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-    public void ConfigureServices(IServiceCollection services)
-    {
-      // Setup EF connection
-      services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration["Data:IEXTrading:ConnectionString"]));
-
-      // added from MVC template
-      services.AddMvc();
-    }
-
-    // this is the version from the MVC template
-    public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-    {
-      //This ensures that the database and tables are created as per the Models.
-      using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
-      {
-        var context = serviceScope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-        context.Database.EnsureCreated();
-      }
-
-      if (env.IsDevelopment())
-      {
-        app.UseBrowserLink();
-        app.UseDeveloperExceptionPage();
-      }
-      else
-      {
-        app.UseExceptionHandler("/Home/Error");
-      }
-
-      app.UseStaticFiles();
-
-      app.UseMvc(routes =>
-      {
-        routes.MapRoute(
-            name: "default",
-            template: "{controller=Home}/{action=Index}/{id?}");
-      });
-    }
-  }
 }
