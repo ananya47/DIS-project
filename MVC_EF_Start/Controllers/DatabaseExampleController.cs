d36aadd2628faab4796f46f2675e38b4ac547ea6
Manish Agrawal <32239632+magrawal23@users.noreply.github.com>
2021-06-14 09:01:45 -0400

    Updated database schema to use 'Id' as primary key, and refer to objects instead of primary keys. Also, included two connection strings to demonstrate adding properties to appsettings.json

--- MVC_EF_Start/Controllers/DatabaseExampleController.cs
+++ MVC_EF_Start/Controllers/DatabaseExampleController.cs
@@ -9,112 +9,110 @@ using MVC_EF_Start.Models;
 
 namespace MVC_EF_Start.Controllers
 {
-  public class DatabaseExampleController : Controller
-  {
-    public ApplicationDbContext dbContext;
-
-    public DatabaseExampleController(ApplicationDbContext context)
+    public class DatabaseExampleController : Controller
     {
-      dbContext = context;
+        public ApplicationDbContext dbContext;
+
+        public DatabaseExampleController(ApplicationDbContext context)
+        {
+            dbContext = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<ViewResult> DatabaseOperations()
+        {
+            // CREATE operation
+            Company MyCompany = new Company();
+            MyCompany.Id = "MCOB";
+            MyCompany.name = "ISM";
+            MyCompany.date = "ISM";
+            MyCompany.isEnabled = true;
+            MyCompany.type = "ISM";
+            MyCompany.iexId = "ISM";
+
+            Quote MyCompanyQuote1 = new Quote();
+            MyCompanyQuote1.date = "11-23-2018";
+            MyCompanyQuote1.open = 46.13F;
+            MyCompanyQuote1.high = 47.18F;
+            MyCompanyQuote1.low = 44.67F;
+            MyCompanyQuote1.close = 47.01F;
+            MyCompanyQuote1.volume = 37654000;
+            MyCompanyQuote1.unadjustedVolume = 37654000;
+            MyCompanyQuote1.change = 1.43F;
+            MyCompanyQuote1.changePercent = 0.03F;
+            MyCompanyQuote1.vwap = 9.76F;
+            MyCompanyQuote1.label = "Nov 23";
+            MyCompanyQuote1.changeOverTime = 0.56F;
+            MyCompanyQuote1.Company = MyCompany;
+
+            Quote MyCompanyQuote2 = new Quote();
+            MyCompanyQuote2.date = "11-23-2018";
+            MyCompanyQuote2.open = 46.13F;
+            MyCompanyQuote2.high = 47.18F;
+            MyCompanyQuote2.low = 44.67F;
+            MyCompanyQuote2.close = 47.01F;
+            MyCompanyQuote2.volume = 37654000;
+            MyCompanyQuote2.unadjustedVolume = 37654000;
+            MyCompanyQuote2.change = 1.43F;
+            MyCompanyQuote2.changePercent = 0.03F;
+            MyCompanyQuote2.vwap = 9.76F;
+            MyCompanyQuote2.label = "Nov 23";
+            MyCompanyQuote2.changeOverTime = 0.56F;
+            MyCompanyQuote2.Company = MyCompany;
+
+            dbContext.Companies.Add(MyCompany);
+            dbContext.Quotes.Add(MyCompanyQuote1);
+            dbContext.Quotes.Add(MyCompanyQuote2);
+
+            dbContext.SaveChanges();
+
+            // READ operation
+            Company CompanyRead1 = dbContext.Companies
+                                    .Where(c => c.Id == "MCOB")
+                                    .First();
+
+            Company CompanyRead2 = dbContext.Companies
+                                    .Include(c => c.Quotes)
+                                    .Where(c => c.Id == "MCOB")
+                                    .First();
+
+            // UPDATE operation
+            CompanyRead1.iexId = "MCOB";
+            dbContext.Companies.Update(CompanyRead1);
+            //dbContext.SaveChanges();
+            await dbContext.SaveChangesAsync();
+
+            // DELETE operation
+            dbContext.Companies.Remove(CompanyRead1);
+            await dbContext.SaveChangesAsync();
+
+            return View();
+        }
+
+        public ViewResult LINQOperations()
+        {
+            Company CompanyRead1 = dbContext.Companies
+                                            .Where(c => c.Id == "MCOB")
+                                            .First();
+
+            Company CompanyRead2 = dbContext.Companies
+                                            .Include(c => c.Quotes)
+                                            .Where(c => c.Id == "MCOB")
+                                            .First();
+
+            Quote Quote1 = dbContext.Companies
+                                    .Include(c => c.Quotes)
+                                    .Where(c => c.Id == "MCOB")
+                                    .FirstOrDefault()
+                                    .Quotes
+                                    .FirstOrDefault();
+
+            return View();
+        }
+
     }
-
-    public IActionResult Index()
-    {
-      return View();
-    }
-
-    public async Task<ViewResult> DatabaseOperations()
-    {
-      // CREATE operation
-      Company MyCompany = new Company();
-      MyCompany.symbol = "MCOB";
-      MyCompany.name = "ISM";
-      MyCompany.date = "ISM";
-      MyCompany.isEnabled = true;
-      MyCompany.type = "ISM";
-      MyCompany.iexId = "ISM";
-
-      Quote MyCompanyQuote1 = new Quote();
-      //MyCompanyQuote1.EquityId = 123;
-      MyCompanyQuote1.date = "11-23-2018";
-      MyCompanyQuote1.open = 46.13F;
-      MyCompanyQuote1.high = 47.18F;
-      MyCompanyQuote1.low = 44.67F;
-      MyCompanyQuote1.close = 47.01F;
-      MyCompanyQuote1.volume = 37654000;
-      MyCompanyQuote1.unadjustedVolume = 37654000;
-      MyCompanyQuote1.change = 1.43F;
-      MyCompanyQuote1.changePercent = 0.03F;
-      MyCompanyQuote1.vwap = 9.76F;
-      MyCompanyQuote1.label = "Nov 23";
-      MyCompanyQuote1.changeOverTime = 0.56F;
-      MyCompanyQuote1.symbol = "MCOB";
-
-      Quote MyCompanyQuote2 = new Quote();
-      //MyCompanyQuote1.EquityId = 123;
-      MyCompanyQuote2.date = "11-23-2018";
-      MyCompanyQuote2.open = 46.13F;
-      MyCompanyQuote2.high = 47.18F;
-      MyCompanyQuote2.low = 44.67F;
-      MyCompanyQuote2.close = 47.01F;
-      MyCompanyQuote2.volume = 37654000;
-      MyCompanyQuote2.unadjustedVolume = 37654000;
-      MyCompanyQuote2.change = 1.43F;
-      MyCompanyQuote2.changePercent = 0.03F;
-      MyCompanyQuote2.vwap = 9.76F;
-      MyCompanyQuote2.label = "Nov 23";
-      MyCompanyQuote2.changeOverTime = 0.56F;
-      MyCompanyQuote2.symbol = "MCOB";
-
-      dbContext.Companies.Add(MyCompany);
-      dbContext.Quotes.Add(MyCompanyQuote1);
-      dbContext.Quotes.Add(MyCompanyQuote2);
-
-      dbContext.SaveChanges();
-      
-      // READ operation
-      Company CompanyRead1 = dbContext.Companies
-                              .Where(c => c.symbol == "MCOB")
-                              .First();
-
-      Company CompanyRead2 = dbContext.Companies
-                              .Include(c => c.Quotes)
-                              .Where(c => c.symbol == "MCOB")
-                              .First();
-
-      // UPDATE operation
-      CompanyRead1.iexId = "MCOB";
-      dbContext.Companies.Update(CompanyRead1);
-      //dbContext.SaveChanges();
-      await dbContext.SaveChangesAsync();
-      
-      // DELETE operation
-      //dbContext.Companies.Remove(CompanyRead1);
-      //await dbContext.SaveChangesAsync();
-
-      return View();
-    }
-
-    public ViewResult LINQOperations()
-    {
-      Company CompanyRead1 = dbContext.Companies
-                                      .Where(c => c.symbol == "MCOB")
-                                      .First();
-
-      Company CompanyRead2 = dbContext.Companies
-                                      .Include(c => c.Quotes)
-                                      .Where(c => c.symbol == "MCOB")
-                                      .First();
-
-      Quote Quote1 = dbContext.Companies
-                              .Include(c => c.Quotes)
-                              .Where(c => c.symbol == "MCOB")
-                              .FirstOrDefault()
-                              .Quotes
-                              .FirstOrDefault();
-
-      return View();
-    }
-
-  }
 }
--- MVC_EF_Start/Models/EF_Models.cs
+++ MVC_EF_Start/Models/EF_Models.cs
@@ -3,38 +3,38 @@ using System.ComponentModel.DataAnnotations;
 
 namespace MVC_EF_Start.Models
 {
-  public class Company
-  {
-    [Key]
-    public string symbol { get; set; }
-    public string name { get; set; }
-    public string date { get; set; }
-    public bool isEnabled { get; set; }
-    public string type { get; set; }
-    public string iexId { get; set; }
-    public List<Quote> Quotes { get; set; }
-  }
+    public class Company
+    {
+        public string Id { get; set; }
+        public string name { get; set; }
+        public string date { get; set; }
+        public bool isEnabled { get; set; }
+        public string type { get; set; }
+        public string iexId { get; set; }
+        public List<Quote> Quotes { get; set; }
+    }
 
-  public class Quote
-  {
-    public int QuoteId { get; set; }
-    public string date { get; set; }
-    public float open { get; set; }
-    public float high { get; set; }
-    public float low { get; set; }
-    public float close { get; set; }
-    public int volume { get; set; }
-    public int unadjustedVolume { get; set; }
-    public float change { get; set; }
-    public float changePercent { get; set; }
-    public float vwap { get; set; }
-    public string label { get; set; }
-    public float changeOverTime { get; set; }
-    public string symbol { get; set; }
-  }
+    public class Quote
+    {
+        public int Id { get; set; }
+        public string date { get; set; }
+        public float open { get; set; }
+        public float high { get; set; }
+        public float low { get; set; }
+        public float close { get; set; }
+        public int volume { get; set; }
+        public int unadjustedVolume { get; set; }
+        public float change { get; set; }
+        public float changePercent { get; set; }
+        public float vwap { get; set; }
+        public string label { get; set; }
+        public float changeOverTime { get; set; }
+        public string ClassDemo { get; set; }
+        public Company Company { get; set; }
+    }
 
-  public class ChartRoot
-  {
-    public Quote[] chart { get; set; }
-  }
+    public class ChartRoot
+    {
+        public Quote[] chart { get; set; }
+    }
 }
