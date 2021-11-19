06adb9143c5dc2b56d983fa154b6cb1fb050f76c
ananya47 <63702545+ananya47@users.noreply.github.com>
2021-11-18 22:04:43 -0500

    final changes for Assignment-4

--- MVC_EF_Start/Controllers/DatabaseExampleController.cs
+++ MVC_EF_Start/Controllers/DatabaseExampleController.cs
@@ -13,6 +13,7 @@ namespace MVC_EF_Start.Controllers
     {
         public ApplicationDbContext dbContext;
 
+
         public DatabaseExampleController(ApplicationDbContext context)
         {
             dbContext = context;
@@ -25,11 +26,11 @@ namespace MVC_EF_Start.Controllers
 
         public async Task<ViewResult> DatabaseOperations()
         {
-            //Assignment 4 
-            //Table 1  - Doctor Records 
+            // Assignment 4
+            //Table 1 - Doctor Records
 
             Doctor DocRec1 = new Doctor();
-           // DocRec1.doctorID = 1;
+            // DocRec1.doctorID = 1;
             DocRec1.docFname = "John";
             DocRec1.docLname = "Smith";
             DocRec1.docAge = 30;
@@ -37,7 +38,7 @@ namespace MVC_EF_Start.Controllers
             DocRec1.docMobile = 8130001234;
 
             Doctor DocRec2 = new Doctor();
-         //   DocRec2.doctorID = 2;
+            //   DocRec2.doctorID = 2;
             DocRec2.docFname = "Emily";
             DocRec2.docLname = "Green";
             DocRec2.docAge = 35;
@@ -45,7 +46,7 @@ namespace MVC_EF_Start.Controllers
             DocRec2.docMobile = 8130005678;
 
             Doctor DocRec3 = new Doctor();
-          //  DocRec3.doctorID = 3;
+            //  DocRec3.doctorID = 3;
             DocRec3.docFname = "Joe";
             DocRec3.docLname = "T";
             DocRec3.docAge = 40;
@@ -53,7 +54,7 @@ namespace MVC_EF_Start.Controllers
             DocRec3.docMobile = 8130009123;
 
             Doctor DocRec4 = new Doctor();
-          //  DocRec4.doctorID = 4;
+            //  DocRec4.doctorID = 4;
             DocRec4.docFname = "Simsons";
             DocRec4.docLname = "Bing";
             DocRec4.docAge = 45;
@@ -61,7 +62,7 @@ namespace MVC_EF_Start.Controllers
             DocRec4.docMobile = 8130004567;
 
             Doctor DocRec5 = new Doctor();
-           // DocRec5.doctorID = 5;
+            // DocRec5.doctorID = 5;
             DocRec5.docFname = "Phoebe";
             DocRec5.docLname = "Buffay";
             DocRec5.docAge = 30;
@@ -77,7 +78,7 @@ namespace MVC_EF_Start.Controllers
 
             //Table 2 - Patient records
             Patient PatRec1 = new Patient();
-           // PatRec1.patientID = 1;
+            // PatRec1.patientID = 1;
             PatRec1.fname = "Ross";
             PatRec1.lname = "Geller";
             PatRec1.age = 43;
@@ -109,7 +110,7 @@ namespace MVC_EF_Start.Controllers
             PatRec4.mobile = 8135937457;
 
             Patient PatRec5 = new Patient();
-           // PatRec5.patientID = 5;
+            // PatRec5.patientID = 5;
             PatRec5.fname = "Mariah";
             PatRec5.lname = "Better Half";
             PatRec5.age = 41;
@@ -152,7 +153,7 @@ namespace MVC_EF_Start.Controllers
             dbContext.Brands.Add(Brand5);
             dbContext.SaveChanges();
 
-            //Table 4 Medicine records 
+            ////Table 4 Medicine records 
             Medicine Med1 = new Medicine();
             //Med1.medID = 1;
             Med1.medName = "Advair Diskus";
@@ -196,7 +197,7 @@ namespace MVC_EF_Start.Controllers
             dbContext.SaveChanges();
 
 
-            //Table 5 - Prescription 
+            ////Table 5 - Prescription 
 
             Prescription Pres1 = new Prescription();
             //Pres1.presID = 1;
@@ -296,30 +297,238 @@ namespace MVC_EF_Start.Controllers
             //    dbContext.Companies.Remove(CompanyRead1);
             //    await dbContext.SaveChangesAsync();
 
-            //    return View();
-            //}
-
-            //public ViewResult LINQOperations()
-            //{
-            //    Company CompanyRead1 = dbContext.Companies
-            //                                    .Where(c => c.Id == "MCOB")
-            //                                    .First();
-
-            //    Company CompanyRead2 = dbContext.Companies
-            //                                    .Include(c => c.Quotes)
-            //                                    .Where(c => c.Id == "MCOB")
-            //                                    .First();
-
-            //    Quote Quote1 = dbContext.Companies
-            //                            .Include(c => c.Quotes)
-            //                            .Where(c => c.Id == "MCOB")
-            //                            .FirstOrDefault()
-            //                            .Quotes
-            //                            .FirstOrDefault();
-
             return View();
-            //}
+        }
+        //Query 1: List the first and last names of all the patients in the database.
+        public ViewResult GetAllPatients()
+        {
+
+            var query1 = from pat in dbContext.Patients
+                               select new { pat.fname, pat.lname };
+            return View(query1);
+        }
+        //Query 2: List the first and last name of the patients who consulted a particular doctor on a given date (Ex: doctor name: “John Smith”, Date: “2021-08-01”).
+        public ViewResult GetPatientsConsultingADoctor()
+        {
+
+            var query2 = from a in dbContext.Appointments
+                         where a.Doctor.doctorID == 5 && a.apptdate == "15-11-2021"
+                         select new { a.Patient.fname, a.Patient.lname };
+            return View(query2);
+        }
+
+        //Query 3: List names of the Generic medicine’s names along with its brand name and price which has unit price greater than $100. (Note: Brand name is the manufacturing company of that medicine).
+
+        public ViewResult GenericMed()
+        {
+            var query3 = from m in dbContext.Medicines
+                         where m.price > 100
+                         select new { m.medName, m.Brand.brandName };
+            return View(query3);
+        }
+        //public ViewResult GetMedicineRecords(int Price, Boolean GenericCategory)
+        //{
+        //    var MedicineRecord = (from Med in dbContext.Medicine
+
+        //                          where Med.Price >= 100 and Med.isGeneric == GenericCategory
+        //        select  new { Med.Name, Med.BrandName, Med.Price }).ToList();
+        //    return View(MedicineRecord);
+        //}
+
+        //Query 4: List the top 5 doctors first and last names along with number of patients visited them, who as the highest patient count.
+        public ViewResult GetTopPatientsCount()
+        {
+            var query4 = from app in dbContext.Appointments
+                         join doc in dbContext.Doctors
+                         on app.Doctor.doctorID equals doc.doctorID
+                         join pt in dbContext.Patients
+                         on app.Patient.patientID equals pt.patientID
+                         select new{ doc.docFname,doc.docLname};
+
+           // var docs = dbContext.Doctors.OrderBy(doc => doc.Patient.Count).Take(5);
+            return View(query4);
+        }
+
+        // Query 5: For a given patient list of all doctors first and last names he visited(Ex: name: ‘John’).
+        public ViewResult doc_data()
+        {
+
+
+
+            var query5 = from a in dbContext.Appointments
+                         where (a.Patient.fname == "Monica" && a.Patient.lname=="Geller")
+                         select new { a.Doctor.docFname, a.Doctor.docLname };
+            return View(query5);
+        }
+
+        public ViewResult GetDocNames()
+        {
+            var query6 = from m in dbContext.Medicines
+                         join pr in dbContext.Prescriptions
+                         on m.medID equals pr.Medicine.medID
+                         join appt in dbContext.Appointments
+                         on pr.presID equals appt.Prescription.presID
+                         join pat in dbContext.Patients
+                         on appt.Patient.patientID equals pat.patientID
+                         join d in dbContext.Doctors
+                         on appt.Doctor.doctorID equals d.doctorID
+                         where (m.medName == "Lyrica")
+
+                         select new { d.docFname, d.docLname };
+
+
+
+            return View(query6);
+
+
 
         }
+
+        public ViewResult DocCountres()
+        {
+            var query4 = from a in dbContext.Appointments
+                         group a by a.Doctor.doctorID into docGroup
+                         select new
+         
+                         {
+                             doctorID = docGroup.Key,
+                             Count = docGroup.Count(),
+                             
+                         } ;
+
+            return View(query4);
+        }
+
+        public ViewResult GetTopPatientsCount1()
+        {
+            var query4 = (from app in dbContext.Appointments
+                          join doc in dbContext.Doctors
+                          on app.Doctor.doctorID equals doc.doctorID
+                          join pt in dbContext.Patients
+                          on app.Patient.patientID equals pt.patientID
+                          group doc by new { doc.doctorID, doc.docFname, doc.docLname } into res
+                          select new
+                          {
+                              DocName = res.Key.docFname
+                          ,
+                              lname = res.Key.docLname
+                          ,
+                              pcnt = res.Count()
+                          });
+            return View(query4);
+          
+        }
+        //public viewresult gettoppatientscount()
+        //{
+        //   var toppatientsdoctorsrecord = (from pr in dbcontext.prescriptions
+        //                                  join dt in dbcontext.doctors on pr.doctorid == dt.doctorid
+        //                   join pt in dbcontext.patient on pr.patientid == pt.patientid
+        //                   group by dt.doctorid
+        //      select  new { dt.firstname, dt.lastname, count(pt.patientid) }).tolist().take(5);
+        //    return view(toppatientsdoctorsrecord);
+        //}
+        //Query 6: List all the doctor first and last names who have prescribed a given medicine to their patients (Ex: medicine name: “Molnupiravir”).
+        //public ViewResult DoctorsVisited()
+        //{
+        //    var PatientRead2 = (from d in dbContext.Doctors
+        //                        join ad in dbContext.Appointments on d.doctorID = ad.doctorID
+        //                   join pd in dbContext.Patients pd on pd.patientID = ad.doctorID
+        //        where pd.patient_fname = 'John'
+        //        select new { d.doc_fname, d.doc_lname }).ToList();
+        //    return View(PatientRead2);
+        //}
+        // Debug.WriteLine(pat.fname + " " + pat.lname);
+
+        //    foreach(Patient pat in dbContext.Patients)
+        //{
+        //    Console.WriteLine(pat.fname + " " + pat.lname);
+        //}
+
+        //    Company CompanyRead1 = dbContext.Companies
+        //                                    .Where(c => c.Id == "MCOB")
+        //                                    .First();
+
+        //    Company CompanyRead2 = dbContext.Companies
+        //                                    .Include(c => c.Quotes)
+        //                                    .Where(c => c.Id == "MCOB")
+        //                                    .First();
+
+        //    Quote Quote1 = dbContext.Companies
+        //                            .Include(c => c.Quotes)
+        //                            .Where(c => c.Id == "MCOB")
+        //                            .FirstOrDefault()
+        //                            .Quotes
+        //                            .FirstOrDefault();
+
+        //public ViewResult GetPatientsConsultingADoctor()
+        //{
+
+        //    var query2 = from a in dbContext.Appointments
+        //                 where a.Doctor.doctorID == 5 && a.apptdate == "15-11-2021"
+        //                 select new { a.Patient.fname, a.Patient.lname };
+        //    return View(query2);
+        //}
+        //Query 3: List names of the Generic medicine’s names along with its brand name and price which has unit price greater than $100. (Note: Brand name is the manufacturing company of that medicine).
+        //public ViewResult GetMedicineRecords(int Price, Boolean GenericCategory)
+        //{
+        //    var MedicineRecord = (from Med in dbContext.Medicine
+
+        //                          where Med.Price >= 100 and Med.isGeneric == GenericCategory
+        //        select  new { Med.Name, Med.BrandName, Med.Price }).ToList();
+        //    return View(MedicineRecord);
+        //}
+
+        //  //Query 4: List the top 5 doctors first and last names along with number of patients visited them, who as the highest patient count.
+        //public viewresult gettoppatientscount()
+        //{
+        //    var toppatientsdoctorsrecord = (from pr in dbcontext.prescriptions
+        //                                    join dt in dbcontext.doctors on pr.doctorid == dt.doctorid
+        //                    join pt in dbcontext.patient on pr.patientid == pt.patientid
+        //                    group by dt.doctorid
+        //        select  new { dt.firstname, dt.lastname, count(pt.patientid) }).tolist().take(5);
+        //    return view(toppatientsdoctorsrecord);
+        //}
+
+
+        //public ViewResult patient_details()
+        //{
+        //    var sample = from md in dbContext.Medicines // outer sequence
+        //                 join bd in dbContext.Brands //inner sequence
+        //                 on md.Brand equals bd.brandID // key selector
+        //                 select new
+        //                 {
+        //                     md.medName,
+        //                     bd.brandName
+        //                 };
+        //    return View(sample);
+        //}
+        //public ViewResult patient_details()
+        //{
+        //    var res = from ad in dbContext.Appointments
+        //              where ad.Doctor.doctorID == 1
+        //              select new { ad.Patient.fname };
+        //    return View(res);
+        //}
+
+        ////Query-6
+        //public ViewResult DocswhopresbMed()
+        //{
+        //    var doc_details = (from d in dbContext.Doctors
+        //                       where d.Appointments.Prescriptions.Medicines.medName == 'Zjj'
+        //                       select new { d.docFname, d.docLname }).ToList();
+        //    return View(doc_details);
+        //}
+        //public ViewResult DocswhopresbMedicien()
+        //{
+        //    var rec = dbContext.Doctors
+        //        .Include(c => dbContext.Patients)
+
+        //        .Where(c => dbContext.Patients.fname == "Ross")
+        //        .Where(c => c.lname == "Geller");
+
+        //    return View(rec);
+        //}
+        //}
     }
 }
+
